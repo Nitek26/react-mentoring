b8ca9e74446c8bc6f2aaea3e532c41affc75569f
Pawel Kwaskiewicz <pawel.kwaskiewicz@gmail.com>
2020-08-24 16:04:49 +0200

    Adding more (...) button

---
+++ src/elements/Library/MovieCover/MoreButton/MoreButton.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './MoreButton.css';
+
+export default (props) => {
+    return <button className={`moreButton ${props.className}`}></button>
+}
--- src/elements/Library/MovieCover/MovieCover.jsx
+++ src/elements/Library/MovieCover/MovieCover.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './MovieCover.css'
 import PropTypes from 'prop-types';
+import MoreButton from './MoreButton/MoreButton'
 
 class MovieCover extends React.Component {
     render() {
         return (
             <div className="movieCover">
+                <MoreButton className="movieMoreButton"></MoreButton>
                 <img src={this.props.movie.poster_path} />
                 <div className="title">{this.props.movie.title}</div>
                 <div className="year">{new Date(this.props.movie.release_date).getFullYear()}</div>
