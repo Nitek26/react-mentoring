fea6845bb7ac269f474014f7d8c01ff58e3b8d5b
Pawel Kwaskiewicz <pawel.kwaskiewicz@gmail.com>
2020-09-30 11:25:16 +0200

    Changing homepage to functional

--- src/elements/Homepage/Homepage.jsx
+++ src/elements/Homepage/Homepage.jsx
@@ -1,32 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Library from './../Library/Library';
 import Search from './../Search/Search';
 import Details from './../Details/Details';
 import Footer from './../Footer/Footer';
-import Modal from './../Modal/Modal';
 import './Homepage.css';
 
-class Homepage extends React.Component {
-    state = {
-        selectedMovie: null
-    }
+export default () => {
+    const [selectedMovie, setSelectedMovie] = useState(null);
 
-    onMovieSelect = (movie) => {
-        console.log("click! ", movie);
-        this.setState({selectedMovie: movie});
-    }
-
-    render() {
-        return (
-            <div className="homepage">
-                {this.state.selectedMovie
-                ? <Details movie={this.state.selectedMovie}/>
-                : <Search />}
-                <Library onMovieSelect={this.onMovieSelect}/>
-                <Footer />
-            </div>
-        );
-    }
-}
-
-export default Homepage;
+    return (
+        <div className="homepage">
+            {selectedMovie
+            ? <Details movie={selectedMovie}/>
+            : <Search />}
+            <Library onMovieSelect={(movie) => {
+                    setSelectedMovie(movie)}
+                }/>
+            <Footer />
+        </div>
+    );
+};
--- src/elements/Library/MovieCover/MovieCover.jsx
+++ src/elements/Library/MovieCover/MovieCover.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import './MovieCover.css'
 import PropTypes from 'prop-types';
 import MoreButton from './MoreButton/MoreButton';
